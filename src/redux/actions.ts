14714c7e5096eb0ebc62ce6d7a5d6b4ec08f55b2
Sergey Babich <sergei.babich2022@yandex.by>
2023-03-03 04:04:13 +0300

    -m "first commit"

---
+++ src/redux/actions.ts
@@ -0,0 +1,56 @@
+export enum ACTIONS_TYPE {
+    CHANGE_CURRENCY_FIELD_TYPE = 'CurrencyExchange/CHANGE_CURRENCY_FIELD_TYPE',
+    CHANGE_CHANGE_ACTION = 'CurrencyExchange/CHANGE_CHANGE_ACTION',
+    CHANGE_CURRENT_CURRENCY = 'CurrencyExchange/CHANGE_CURRENT_CURRENCY',
+}
+
+
+export type ChangeCurrencyFieldType = {
+    type: ACTIONS_TYPE.CHANGE_CURRENCY_FIELD_TYPE,
+    amountOfBYN: string
+    amountOfCurrency: string
+};
+
+// @ts-ignore
+export const ChangeCurrencyFieldAC = (amountOfBYN: string, amountOfCurrency: string): ChangeCurrencyFieldType => {
+    return {
+        type: ACTIONS_TYPE.CHANGE_CURRENCY_FIELD_TYPE,
+        amountOfBYN,
+        amountOfCurrency,
+    }
+};
+
+
+
+export type ChangeAction = {
+    type: ACTIONS_TYPE.CHANGE_CHANGE_ACTION
+    isBuying: boolean
+};
+
+// @ts-ignore
+export const ChangeActionAC = (isBuying: boolean): ChangeAction => {
+    return {
+        type: ACTIONS_TYPE.CHANGE_CHANGE_ACTION,
+        isBuying
+    }
+};
+
+
+
+export type ChangeCurrentCurrencyType = {
+    type: ACTIONS_TYPE.CHANGE_CURRENT_CURRENCY
+    currentCurrency: string
+};
+
+// @ts-ignore
+export const СhangeCurrentCurrencyAC = (currentCurrency: string): ChangeCurrentCurrencyType => {
+    return {
+        type: ACTIONS_TYPE.CHANGE_CURRENT_CURRENCY,
+        currentCurrency
+    }
+};
+
+
+
+
+export type CurrencyReducersTypes = ChangeCurrencyFieldType | ChangeAction | ChangeCurrentCurrencyType;
